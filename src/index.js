86f975fb264cbcdc967dd16aeeed90a505ac9713
Gabriel Henrique <gabriel.sanches170@gmail.com>
2022-12-01 23:16:25 -0300

    feat: change repositories/:id/like route, and add likes

--- src/index.js
+++ src/index.js
@@ -76,7 +76,7 @@ app.delete("/repositories/:id", (request, response) => {
 app.post("/repositories/:id/like", (request, response) => {
   const { id } = request.params;
 
-  repositoryIndex = repositories.findIndex(
+  const repositoryIndex = repositories.findIndex(
     (repository) => repository.id === id
   );
 
@@ -86,7 +86,7 @@ app.post("/repositories/:id/like", (request, response) => {
 
   const likes = ++repositories[repositoryIndex].likes;
 
-  return response.json("likes");
+  return response.json({ likes });
 });
 
 module.exports = app;
